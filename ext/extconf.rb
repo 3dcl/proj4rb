c213a01bb88f8221bbb9aba0b215d08b245f6b55
Jan Klimke <jan.klimke@hpi.de>
2019-11-13 18:01:15 +0100

    update extconf file

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,16 +1,17 @@
+# frozen_string_literal: true
+
 require 'mkmf'
 
 dir_config('proj')
 
-if not have_header('proj_api.h', 'definitions.h')
+unless have_header('proj_api.h', 'definitions.h')
   raise('Cannot find proj_api.h header')
 end
 
 have_header('projects.h')
 
-unless have_library('proj', 'pj_init') or
-       have_library('libproj', 'pj_init')
-       
+unless have_library('proj', 'pj_init', 'definitions.h') ||
+       have_library('libproj', 'pj_init', 'definitions.h')
   raise('Cannot find proj4 library')
 end
 
